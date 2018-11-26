dd75707ee87415a52cc634b34de8f476f50f28e6
Vinoth Pandian <vinothpandian@live.com>
2018-11-26 17:39:37 +0100

    nwb update

--- nwb.config.js
+++ nwb.config.js
@@ -1,12 +1,26 @@
-module.exports = {
-  type: 'react-component',
-  npm: {
-    esModules: true,
-    umd: {
-      global: 'SvgSketchCanvas',
-      externals: {
-        react: 'React'
-      }
-    }
+module.exports = function ({ command }) {
+  const config = {
+    type: 'react-component',
+    npm: {
+      esModules: true,
+      umd: {
+        global: 'SvgSketchCanvas',
+        externals: {
+          react: 'React',
+        },
+      },
+    },
+  };
+  // Only include react-hot-loader config when serving a development build
+  if (command.startsWith('serve')) {
+    config.babel = { plugins: 'react-hot-loader/babel' };
+    config.webpack = {
+      config(webpackConfig) {
+        // React Hot Loader's patch module needs to run before your app
+        webpackConfig.entry.unshift('react-hot-loader/patch');
+        return webpackConfig;
+      },
+    };
   }
-}
+  return config;
+};
