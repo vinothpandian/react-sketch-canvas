286a71e7a7ba3bd6493e9d86c8fb7c4d39b317d9
Vinoth Pandian <vinothpandian@live.com>
2021-10-19 18:24:43 +0200

    fix tests and add test to check props

--- cypress/support/index.d.ts
+++ cypress/support/index.d.ts
@@ -2,11 +2,7 @@
 
 declare namespace Cypress {
   interface Chainable<Subject> {
-    /**
-     * Draw a square on canvas
-     * @example
-     * cy.drawSquare(100)
-     */
     drawSquare(side: number): Chainable<any>;
+    getCanvas(): Chainable<any>;
   }
 }
