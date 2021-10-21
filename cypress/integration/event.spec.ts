26b7854493aa7b7af679ba4797c81c21f6510eb0
Vinoth Pandian <vinothpandian@live.com>
2021-10-21 19:12:20 +0200

    test undo, redo, clear and reset canvas

--- cypress/integration/event.spec.ts
+++ cypress/integration/event.spec.ts
@@ -34,3 +34,141 @@ it('should trigger erase mode and add a mask for erasing previous strokes', () =
   cy.get('mask#eraser-mask-0').find('use').should('have.length', 3); // background + two mask paths
   cy.get('mask#eraser-mask-1').find('use').should('have.length', 2); // background + one mask path
 });
+
+describe('undo', () => {
+  it('should undo a stroke', () => {
+    cy.getCanvas().find('path').should('have.length', 0);
+    cy.drawSquare(100, 100, 50, 'pen');
+    cy.getCanvas().find('path').should('have.length', 1);
+
+    cy.findByRole('button', { name: /undo/i }).click();
+    cy.getCanvas().find('path').should('have.length', 0);
+  });
+
+  it('should undo an eraser stroke', () => {
+    cy.drawSquare(100, 100, 50, 'pen');
+    cy.findByRole('button', { name: /eraser/i }).click();
+    cy.drawSquare(100, 150, 50, 'pen');
+
+    cy.getCanvas().find('path').should('have.length', 2);
+    cy.get('#eraser-stroke-group').find('path').should('have.length', 1);
+    cy.get('mask#eraser-mask-0').find('use').should('have.length', 2); // background + one mask path
+
+    cy.findByRole('button', { name: /undo/i }).click();
+    cy.getCanvas().find('path').should('have.length', 1);
+    cy.get('#eraser-stroke-group').find('path').should('have.length', 0);
+  });
+});
+
+describe('redo', () => {
+  it('should redo a stroke', () => {
+    cy.getCanvas().find('path').should('have.length', 0);
+    cy.drawSquare(100, 100, 50, 'pen');
+    cy.getCanvas().find('path').should('have.length', 1);
+
+    cy.findByRole('button', { name: /undo/i }).click();
+    cy.getCanvas().find('path').should('have.length', 0);
+
+    cy.findByRole('button', { name: /redo/i }).click();
+    cy.getCanvas().find('path').should('have.length', 1);
+  });
+
+  it('should redo an eraser stroke', () => {
+    cy.drawSquare(100, 100, 50, 'pen');
+    cy.findByRole('button', { name: /eraser/i }).click();
+    cy.drawSquare(100, 150, 50, 'pen');
+
+    cy.getCanvas().find('path').should('have.length', 2);
+    cy.get('#eraser-stroke-group').find('path').should('have.length', 1);
+    cy.get('mask#eraser-mask-0').find('use').should('have.length', 2); // background + one mask path
+
+    cy.findByRole('button', { name: /undo/i }).click();
+    cy.getCanvas().find('path').should('have.length', 1);
+    cy.get('#eraser-stroke-group').find('path').should('have.length', 0);
+    cy.get('mask#eraser-mask-0').should('not.exist');
+
+    cy.findByRole('button', { name: /redo/i }).click();
+    cy.getCanvas().find('path').should('have.length', 2);
+    cy.get('#eraser-stroke-group').find('path').should('have.length', 1);
+    cy.get('mask#eraser-mask-0').find('use').should('have.length', 2); // background + one mask path
+  });
+});
+
+describe('clearCanvas', () => {
+  it('should clearCanvas but still keep the stack', () => {
+    cy.getCanvas().find('path').should('have.length', 0);
+    cy.drawSquare(100, 100, 50, 'pen');
+    cy.getCanvas().find('path').should('have.length', 1);
+
+    cy.findByRole('button', { name: /clear all/i }).click();
+    cy.getCanvas().find('path').should('have.length', 0);
+
+    cy.findByRole('button', { name: /redo/i }).click();
+    cy.getCanvas().find('path').should('have.length', 0);
+
+    cy.findByRole('button', { name: /undo/i }).click();
+    cy.getCanvas().find('path').should('have.length', 1);
+  });
+
+  it('should clearCanvas with an eraser stroke but still keep the stack', () => {
+    cy.drawSquare(100, 100, 50, 'pen');
+    cy.findByRole('button', { name: /eraser/i }).click();
+    cy.drawSquare(100, 150, 50, 'pen');
+
+    cy.getCanvas().find('path').should('have.length', 2);
+    cy.get('#eraser-stroke-group').find('path').should('have.length', 1);
+    cy.get('mask#eraser-mask-0').find('use').should('have.length', 2); // background + one mask path
+
+    cy.findByRole('button', { name: /clear all/i }).click();
+    cy.getCanvas().find('path').should('have.length', 0);
+    cy.get('mask#eraser-mask-0').should('not.exist');
+
+    cy.findByRole('button', { name: /redo/i }).click();
+    cy.getCanvas().find('path').should('have.length', 0);
+    cy.get('mask#eraser-mask-0').should('not.exist');
+
+    cy.findByRole('button', { name: /undo/i }).click();
+    cy.getCanvas().find('path').should('have.length', 2);
+    cy.get('#eraser-stroke-group').find('path').should('have.length', 1);
+    cy.get('mask#eraser-mask-0').find('use').should('have.length', 2); // background + one mask path
+  });
+});
+
+describe.only('resetCanvas', () => {
+  it('should resetCanvas and remove the stack', () => {
+    cy.getCanvas().find('path').should('have.length', 0);
+    cy.drawSquare(100, 100, 50, 'pen');
+    cy.getCanvas().find('path').should('have.length', 1);
+
+    cy.findByRole('button', { name: /reset all/i }).click();
+    cy.getCanvas().find('path').should('have.length', 0);
+
+    cy.findByRole('button', { name: /redo/i }).click();
+    cy.getCanvas().find('path').should('have.length', 0);
+
+    cy.findByRole('button', { name: /undo/i }).click();
+    cy.getCanvas().find('path').should('have.length', 0);
+  });
+
+  it('should resetCanvas with an eraser stroke and remove the stack', () => {
+    cy.drawSquare(100, 100, 50, 'pen');
+    cy.findByRole('button', { name: /eraser/i }).click();
+    cy.drawSquare(100, 150, 50, 'pen');
+
+    cy.getCanvas().find('path').should('have.length', 2);
+    cy.get('#eraser-stroke-group').find('path').should('have.length', 1);
+    cy.get('mask#eraser-mask-0').find('use').should('have.length', 2); // background + one mask path
+
+    cy.findByRole('button', { name: /reset all/i }).click();
+    cy.getCanvas().find('path').should('have.length', 0);
+    cy.get('mask#eraser-mask-0').should('not.exist');
+
+    cy.findByRole('button', { name: /redo/i }).click();
+    cy.getCanvas().find('path').should('have.length', 0);
+    cy.get('mask#eraser-mask-0').should('not.exist');
+
+    cy.findByRole('button', { name: /undo/i }).click();
+    cy.getCanvas().find('path').should('have.length', 0);
+    cy.get('mask#eraser-mask-0').should('not.exist');
+  });
+});
