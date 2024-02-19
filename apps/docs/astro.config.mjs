d64e648f746d57fd5a3ac77431267f4456267b37
Vinoth Pandian <vinothpandian@users.noreply.github.com>
2024-02-19 11:59:51 -0500

    feat: improve homepage

--- apps/docs/astro.config.mjs
+++ apps/docs/astro.config.mjs
@@ -1,43 +1,54 @@
 import { defineConfig } from "astro/config";
 import starlight from "@astrojs/starlight";
 import tailwind from "@astrojs/tailwind";
-
 import react from "@astrojs/react";
+import starlightTypeDoc, { typeDocSidebarGroup } from "starlight-typedoc";
+
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://react-sketch-canvas.vercel.app/',
   integrations: [
     starlight({
       title: "React Sketch Canvas",
       customCss: [
         "./src/styles/tailwind.css",
         "./src/styles/customize.css",
-        "@fontsource/kalam/400.css",
+        "@fontsource/kalam/400.css"
       ],
       social: {
-        github: "https://github.com/vinothpandian/react-sketch-canvas",
+        github: "https://github.com/vinothpandian/react-sketch-canvas"
       },
+      plugins: [
+        // Generate the documentation.
+        starlightTypeDoc({
+          entryPoints: ["../../packages/react-sketch-canvas/src/index.tsx"],
+          tsconfig: "../../packages/react-sketch-canvas/tsconfig.json"
+        })
+      ],
       sidebar: [
         {
           label: "Guides",
           items: [
             {
               label: "Example Guide",
-              link: "/guides/example/",
-            },
-          ],
+              link: "/guides/example/"
+            }
+          ]
         },
         {
           label: "Reference",
           autogenerate: {
-            directory: "reference",
-          },
+            directory: "reference"
+          }
         },
-      ],
+        // Add the generated sidebar group to the sidebar.
+        typeDocSidebarGroup
+      ]
     }),
     tailwind({
-      applyBaseStyles: false,
+      applyBaseStyles: false
     }),
-    react(),
-  ],
+    react()
+  ]
 });
--- apps/docs/src/components/HomePageDemo.tsx
+++ apps/docs/src/components/HomePageDemo.tsx
@@ -1,9 +1,20 @@
-import { ReactSketchCanvas } from "react-sketch-canvas";
+import {
+  ReactSketchCanvas,
+  type ReactSketchCanvasRef,
+} from "react-sketch-canvas";
+import { type ChangeEvent, useEffect, useRef, useState } from "react";
+import { IconEraser, IconPencil, IconRestore } from "@tabler/icons-react";
 import paths from "../assets/initialSketch.json";
-import { useEffect, useRef } from "react";
+
+const iconButton =
+  "p-2 rounded-xl border cursor-pointer dark:border-accent-900 dark:text-accent-200";
+const defaultIconButton =
+  "bg-transparent hover:bg-accent-100 dark:hover:bg-accent-800";
 
 export function HomePageDemo() {
-  const ref = useRef<ReactSketchCanvas>(null);
+  const [eraser, setEraser] = useState(false);
+  const ref = useRef<ReactSketchCanvasRef>(null);
+  const [strokeColor, setStrokeColor] = useState("#6497eb");
 
   useEffect(() => {
     if (ref.current) {
@@ -11,13 +22,87 @@ export function HomePageDemo() {
     }
   }, []);
 
+  const handleEraserClick = () => {
+    setEraser(true);
+    if (ref.current) {
+      ref.current.eraseMode(true);
+    }
+  };
+
+  const handlePencilClick = () => {
+    setEraser(false);
+    if (ref.current) {
+      ref.current.eraseMode(false);
+    }
+  };
+
+  const handleResetClick = () => {
+    if (ref.current) {
+      ref.current.resetCanvas();
+    }
+  };
+
+  const pencilSelected = !eraser
+    ? "bg-accent-600 text-accent-50 hover:bg-accent-600 hover:text-accent-50"
+    : defaultIconButton;
+
+  const eraserSelected = eraser
+    ? "bg-accent-600 text-accent-50 hover:bg-accent-600 hover:text-accent-50"
+    : defaultIconButton;
+
+  const onColorChange = (event: ChangeEvent<HTMLInputElement>) => {
+    setStrokeColor(event.target.value);
+  };
+
   return (
-    <ReactSketchCanvas
-      ref={ref}
-      canvasColor="transparent"
-      height="400px"
-      strokeWidth={4}
-      strokeColor="red"
-    />
+    <div className="flex gap-4">
+      <ReactSketchCanvas
+        ref={ref}
+        canvasColor="transparent"
+        height="400px"
+        strokeWidth={4}
+        strokeColor={strokeColor}
+      />
+      <div className="flex flex-col w-10">
+        <div className="w-auto h-9 rounded-full overflow-hidden">
+          <input
+            title="Color"
+            className="w-[200%] h-[200%] bg-transparent border-none cursor-pointer appearance-none transform-cpu -translate-x-1/4 -translate-y-1/4"
+            type="color"
+            value={strokeColor}
+            onChange={onColorChange}
+          />
+        </div>
+        <hr />
+        <button
+          title="Pencil"
+          className={`${iconButton} ${pencilSelected}`}
+          type="button"
+          aria-label="pencil"
+          onClick={handlePencilClick}
+        >
+          <IconPencil />
+        </button>
+        <button
+          title="Eraser"
+          className={`${iconButton} ${eraserSelected}`}
+          type="button"
+          aria-label="eraser"
+          onClick={handleEraserClick}
+        >
+          <IconEraser />
+        </button>
+        <hr />
+        <button
+          title="Reset"
+          className={`${iconButton} ${defaultIconButton}`}
+          type="button"
+          aria-label="clear"
+          onClick={handleResetClick}
+        >
+          <IconRestore />
+        </button>
+      </div>
+    </div>
   );
 }
