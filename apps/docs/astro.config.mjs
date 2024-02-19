31a86ca1ea8d9726fc94896290e462d0149f9cda
Vinoth Pandian <vinothpandian@users.noreply.github.com>
2024-02-19 10:34:45 -0500

    feat: customize design

--- apps/docs/astro.config.mjs
+++ apps/docs/astro.config.mjs
@@ -6,25 +6,38 @@ import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [starlight({
-    title: "React Sketch Canvas",
-    customCss: ["./src/tailwind.css"],
-    social: {
-      github: "https://github.com/vinothpandian/react-sketch-canvas"
-    },
-    sidebar: [{
-      label: "Guides",
-      items: [{
-        label: "Example Guide",
-        link: "/guides/example/"
-      }]
-    }, {
-      label: "Reference",
-      autogenerate: {
-        directory: "reference"
-      }
-    }]
-  }), tailwind({
-    applyBaseStyles: false
-  }), react()]
+  integrations: [
+    starlight({
+      title: "React Sketch Canvas",
+      customCss: [
+        "./src/styles/tailwind.css",
+        "./src/styles/customize.css",
+        "@fontsource/kalam/400.css",
+      ],
+      social: {
+        github: "https://github.com/vinothpandian/react-sketch-canvas",
+      },
+      sidebar: [
+        {
+          label: "Guides",
+          items: [
+            {
+              label: "Example Guide",
+              link: "/guides/example/",
+            },
+          ],
+        },
+        {
+          label: "Reference",
+          autogenerate: {
+            directory: "reference",
+          },
+        },
+      ],
+    }),
+    tailwind({
+      applyBaseStyles: false,
+    }),
+    react(),
+  ],
 });
