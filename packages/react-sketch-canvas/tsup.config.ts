7050242d46f28e8a9fc2e86350d4b3d019c812f3
Vinoth Pandian <vinothpandian@users.noreply.github.com>
2026-05-16 21:15:14 -0400

    chore: tighten package publish contents

--- packages/react-sketch-canvas/tsup.config.ts
+++ packages/react-sketch-canvas/tsup.config.ts
@@ -4,10 +4,12 @@ export default defineConfig((options) => {
 	const isProdEnv = options.env?.NODE_ENV === "production";
 
 	return {
+		entry: ["src/index.tsx"],
 		sourcemap: !isProdEnv,
 		format: isProdEnv ? ["esm", "cjs", "iife"] : ["esm"],
 		dts: true,
 		minify: isProdEnv,
 		watch: !isProdEnv,
+		clean: true,
 	};
 });
